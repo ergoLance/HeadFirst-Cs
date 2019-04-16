60a1aa084caa45445d8e597204a8ab60b34b282a
Ergo <39900870+ergoLance@users.noreply.github.com>
2019-04-16 15:30:54 +0300

    Update MainPage.xaml.cs

--- Variables/Variables/MainPage.xaml.cs
+++ Variables/Variables/MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 namespace Variables
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame. fuck yeah
     /// </summary>
     public sealed partial class MainPage : Page
     {
