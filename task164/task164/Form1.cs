29c60e5ce8b1c40cd32ea99b89a70ff942f457e1
Ergo <39900870+ergoLance@users.noreply.github.com>
2019-04-18 22:38:18 +0300

    new task

---
+++ task164/task164/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task164
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            joe = new Guy();
+            joe.cash = 200;
+            joe.name = "Joe";
+
+            bob = new Guy()
+            {
+                cash = 100,
+                name = "Bob"
+            };
+
+            UpdateForm();
+        }
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.name + " have $" + joe.cash;
+            bobsCashLabel.Text = bob.name + " have $" + bob.cash;
+            bankCashLabel.Text = "Bank have $" + bank;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bank is out of money");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void JoeGivesToBob_Click(object sender, EventArgs e)
+        {
+            bob.cash += joe.GiveCash(10);
+            UpdateForm();
+        }
+
+        private void BobGivesToJoe_Click(object sender, EventArgs e)
+        {
+            joe.cash += bob.GiveCash(5);
+            UpdateForm();
+        }
+    }
+}
---
+++ task164/task164/Guy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task164
+{
+    class Guy
+    {
+        public string name;
+        public int cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= cash && amount > 0)
+            {
+                cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I dont have enough money " + amount, name + " telling...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " dont need", name + " telling...");
+                return 0;
+            }
+        }
+        
+
+        
+    }
+}
